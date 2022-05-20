03f2c194d7731d7709846475cbd45250c02cb48a
officialesse <uzomaesse@gmail.com>
2022-05-20 18:55:25 +0100

    final changes

--- js/menulis.js
+++ js/menulis.js
@@ -13,8 +13,8 @@ const speakersData = [
         h5: 'Rage Against the Machine',
         h6: 'Los Angeles, California, United States',
         description: 'Formed in 1991, the group consists of vocalist Zack de la Rocha, bassist and backing vocalist Tim Commerford, guitarist Tom Morello, and drummer Brad Wilk.',
-        featured_image1: "../assets/img/lineup/rage.jpg",
-        featured_image2: "../assets//img/gray-squares.png",
+        featured_image1: "https://i.ibb.co/ZJP0mHk/rage.jpg",
+        featured_image2: "https://i.ibb.co/CMxyDMK/gray-squares.png",
         live_link: 'https://genius.com/artists/Rage-against-the-machine'
     },
 
@@ -24,17 +24,17 @@ const speakersData = [
         h5: 'Travis Scott',
         h6: 'Houston, Texas, United States',
         description: 'Jacques Berman Webster II, known professionally as Travis Scott, is an American rapper, singer, songwriter, and record producer.',
-        featured_image1: "../assets/img/lineup/travis.jpg",
-        featured_image2: "../assets//img/gray-squares.png",
-        live_link: "https://genius.com/artists/Travis-scott"
+        featured_image1: "https://i.ibb.co/ZJP0mHk/rage.jpg",
+        featured_image2: "https://i.ibb.co/CMxyDMK/gray-squares.png",
+        live_link: "https://i.ibb.co/CMxyDMK/gray-squares.png"
     },
     {
         id: '3',
         h5: 'Frank Ocean',
         h6: 'Long Beach, California, United States',
         description: 'He began his musical career as a ghostwriter, prior to joining hip hop collective Odd Future in 2010. In 2011, Ocean released his critically successful debut mixtape Nostalgia,Ultra',
-        featured_image1: "../assets/img/lineup/frank.jpg",
-        featured_image2: "../assets//img/gray-squares.png",
+        featured_image1: "https://i.ibb.co/ZJP0mHk/rage.jpg",
+        featured_image2: "https://i.ibb.co/CMxyDMK/gray-squares.png",
         live_link: 'https://genius.com/artists/Frank-ocean'
     },
     {
@@ -42,8 +42,8 @@ const speakersData = [
         h5: 'Calvin Harris',
         h6: 'Dumfries, , United Kingdom',
         description: 'Adam Richard Wiles, known professionally as Calvin Harris, is a Scottish DJ, record producer, singer, and songwriter. He is known for his singles "We Found Love", "This Is What You Came For", "Summer", "Feel So Close", "Feels", and “One Kiss',
-        featured_image1: "../assets/img/lineup/calvin.jpg",
-        featured_image2: "../assets//img/gray-squares.png",
+        featured_image1: "https://i.ibb.co/ZJP0mHk/rage.jpg",
+        featured_image2: "https://i.ibb.co/CMxyDMK/gray-squares.png",
         live_link: 'https://genius.com/artists/Calvin-harris'
     },
 
@@ -52,8 +52,8 @@ const speakersData = [
         h5: 'Lana del Rey',
         h6: 'Manhattan, New York, United States',
         description: 'Elizabeth Woolridge Grant, known professionally as Lana Del Rey, is an American singer,songwriter, musician and record producer',
-        featured_image1: "../assets/img/lineup/calvin.jpg",
-        featured_image2: "../assets//img/gray-squares.png",
+        featured_image1: " https://i.ibb.co/WnxQ4rs/travis.jpg",
+        featured_image2: "https://i.ibb.co/CMxyDMK/gray-squares.png",
         live_link: 'https://genius.com/artists/Lana-del-rey'
     },
 
@@ -62,8 +62,8 @@ const speakersData = [
         h5: 'Tom Yorke',
         h6: 'Wellingborough, United Kingdom',
         description: 'Thomas Edward Yorke is an English musician and the main vocalist and songwrit    the rock band Radiohead. A multi-instrumentalist, he mainly plays guitar and keyboards nd is known for his falsetto.',
-        featured_image1: "../assets/img/lineup/yorke.jpg",
-        featured_image2: "../assets//img/gray-squares.png",
+        featured_image1: "https://i.ibb.co/WnxQ4rs/travis.jpg",
+        featured_image2: "https://i.ibb.co/CMxyDMK/gray-squares.png",
         live_link: 'https://genius.com/artists/Thom-yorke'
     },
 
